a7725f733adf81582a16834b246d66a2c04bd452
Michael Weigle <michael.weigle@gmail.com>
2015-06-06 20:14:11 +0000

    category filtering

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,26 @@
 class EventsController < ApplicationController
     def index
-        @events = load_events
+        @events = filtered_categories(params["Category"]) if params["Category"]
+        @categories = all_categories
+        @events = load_events(all_categories) unless params["Category"]
     end
     
-    def load_events
-        params = { category: '1',
+    def all_categories
+        MeetupApi.new.categories({ format: 'json', page: '50', omit: 'shortname' })
+    end
+    
+    def filtered_categories(filters)
+        params = { category: filters,
+          zip: '20009',
+          status: 'upcoming',
+          format: 'json',
+          page: '50'}
+        meetup_api = MeetupApi.new
+        @events = meetup_api.open_events(params)
+    end
+    
+    def load_events(filters=all_categories)
+        params = { category: all_categories["results"].collect { |c| c["id"] }.join(','),
           zip: '20009',
           status: 'upcoming',
           format: 'json',
